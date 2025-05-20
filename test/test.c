cf0541b751ff4920611d32cdba010195dda5e939
Maieul BOYER <maieul.dev@familleboyer.net>
2025-05-20 19:34:17 +0200

    better test for strdup.s

--- test/test.c
+++ test/test.c
@@ -6,7 +6,7 @@
 /*   By: maiboyer <maiboyer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/18 22:46:50 by maiboyer          #+#    #+#             */
-/*   Updated: 2025/05/20 17:25:29 by maiboyer         ###   ########.fr       */
+/*   Updated: 2025/05/20 19:32:39 by maiboyer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,52 +17,19 @@
 #include <stdlib.h>
 #include <string.h>
 
-char *debug_string(const char *s, size_t n) {
-  char *out = malloc(n * 4);
-  assert(out != NULL);
-  char *h = out;
-  for (size_t i = 0; i < n; i++) {
-    if (s[i] == '\0') {
-      *(h++) = '\\';
-      *(h++) = '0';
-    } else if (s[i] == '\\') {
-      *(h++) = '\\';
-      *(h++) = '\\';
-    } else if (s[i] == '\n') {
-      *(h++) = '\\';
-      *(h++) = 'n';
-    } else if (s[i] > '~' || s[i] < ' ') {
-      *(h++) = '\\';
-      *(h++) = 'x';
-      *(h++) = "0123456789ABCDEF"[(s[i] & 0xF0) >> 4];
-      *(h++) = "0123456789ABCDEF"[(s[i] & 0x0F)];
-    } else
-      *(h++) = s[i];
-  }
-  *h = 0;
-  return out;
+void test_strcpy() {
+
+  char *s = "Some test that isn't just a.";
+  char *out = malloc(ft_strlen(s) + 1);
+
+  ft_strcpy(out, s);
+  assert(strcmp(s, out) == 0);
+  free(out);
 }
 
-#define DO_TEST(s)                                                             \
-  do {                                                                         \
-    char *str = strdup(s);                                                     \
-    char mbuffer[1024];                                                        \
-    char lbuffer[1024];                                                        \
-    memset(mbuffer, '@', sizeof(mbuffer));                                     \
-    memset(lbuffer, '@', sizeof(lbuffer));                                     \
-    uint64_t l = ft_strlen(str);                                                  \
-    ft_strcpy(mbuffer, str);                                            \
-    strcpy(lbuffer, str);                                               \
-    if (memcmp(mbuffer, lbuffer, 1024) == 0)                                   \
-      printf("[\x1b[32mSUCCESS\x1b[0m]\n");                                    \
-    else {                                                                     \
-      char *ms = debug_string(mbuffer, sizeof(mbuffer));                       \
-      char *ls = debug_string(lbuffer, sizeof(lbuffer));                       \
-      printf("[\x1b[31mFAILURE\x1b[0m]\nMINE: '%s'\nLIBC:'%s'\n", ms, ls);     \
-      free(ms);                                                                \
-      free(ls);                                                                \
-    };                                                                         \
-    free(str);                                                                 \
-  } while (0);
-
-int main(void) { DO_TEST("some short text"); }
+int main(void) {
+  char *s1 = "0123456789ABCDE";
+  char *s2 = ft_strdup(s1);
+  assert(strcmp(s1, s2) == 0);
+  free(s2);
+}
